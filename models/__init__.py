f3716d95ffc77c66fefb8d8bbd1dc4c1e09d7ce7
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-08-13 13:16:18 +0100

    AirBnB_clone

--- models/__init__.py
+++ models/__init__.py
@@ -2,8 +2,10 @@
 """create a unique FileStorage instance for your application."""
 
 
-from models.engine import file_storage
-storage = file_storage.FileStorage()
-storage.reload
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
 
 
--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ Returns a dictionary containing all keys/values of __dict__ of the instance."""
 
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel:
@@ -36,19 +36,19 @@ class BaseModel:
                     pass
                 else:
                     setattr(self, k, v)
-            storage.new(self)
+            models.storage.new(self)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def save(self):
 
         """Updates the public instance attribute updated_at."""
 
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
 
